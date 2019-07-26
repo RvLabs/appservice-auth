24501eaf181b8670426e4c6393e80f822fb0662c
Romer Ventura <roventur@microsoft.com>
2019-07-26 11:07:32 -0500

    post logout url change

--- AppServiceEasyAuth/FrontendAngular/src/app/profiles/profile-data.service.ts
+++ AppServiceEasyAuth/FrontendAngular/src/app/profiles/profile-data.service.ts
@@ -27,6 +27,6 @@ export class ProfileDataService {
   }
 
   logOut(): Observable<any> {
-    return this.http.get('/.auth/logout?post_logout_redirect_uri=/index.html', {observe: 'response'});
+    return this.http.get('/.auth/logout?post_logout_redirect_uri=/', {observe: 'response'});
   }
 }
